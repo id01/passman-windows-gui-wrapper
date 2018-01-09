9a06d4fccdcdee11dc51929760b05382ac3fc40d
id01 <gaviniboom@gmail.com>
2018-01-08 17:28:58 -0800

    Add generic configuration

--- windows_gui/Config.cs
+++ windows_gui/Config.cs
@@ -8,7 +8,7 @@ namespace WindowsFormsApp1
 {
     class Config
     {
-        public static string serverURL = "https://passwords.id0.one"; // URL with port only, no URI
+        public static string serverURL = "http://localhost:5000"; // URL with port only, no URI
         public static string serverURI = ""; // No slash at the end
     }
 }
